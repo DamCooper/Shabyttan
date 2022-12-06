ee2481ad2e994fd8ba277af0ebb4668ad81e8f95
inkar <inkarnash@gmail.com>
2022-12-06 12:09:06 +0600

    adding FavoriteDatabase.kt

---
+++ app/src/main/java/com/example/shabyttan/FavoriteDatabase.kt
@@ -0,0 +1,40 @@
+package com.example.shabyttan
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Favorite::class],
+    version = 1,
+    exportSchema = true
+)
+abstract class FavoriteDatabase : RoomDatabase() {
+
+    abstract fun favoriteDao(): FavoriteDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: FavoriteDatabase? = null
+
+        fun getDatabase(context: Context): FavoriteDatabase {
+            if (INSTANCE == null) {
+                synchronized(this) {
+                    INSTANCE = buildDatabase(context)
+                }
+            }
+            return INSTANCE!!
+        }
+
+        private fun buildDatabase(context: Context): FavoriteDatabase {
+            return Room.databaseBuilder(
+                context.applicationContext,
+                FavoriteDatabase::class.java,
+                "favorites_database"
+            )
+                .build()
+        }
+    }
+}
